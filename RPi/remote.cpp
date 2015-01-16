ac4b01792ff10200872d0909ccb3e66ab9c8c97f
Tim Galvin <tgalvin@uw.edu>
2015-01-15 20:54:01 -0800

    Updated gpio pin nums

--- RPi/remote.cpp
+++ RPi/remote.cpp
@@ -7,12 +7,12 @@
 
 using namespace std;
 //RF24 radio("/dev/spidev0.0",8000000 , 25);  
-//RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+RF24 radio(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+//RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 
-RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
 //const int role_pin = 7;
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-//const uint8_t pipes[][6] = {"1Node","2Node"};
+
 
 // hack to avoid SEG FAULT, issue #46 on RF24 github https://github.com/TMRh20/RF24.git
 unsigned long  got_message;
